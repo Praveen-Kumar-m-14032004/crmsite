43809267f3172884f90f70357eebac211be4eca0
Praveen Kumar M <praveenkumar.m@quadrasystems.net>
2026-09-14 19:14:38 +0530

    Remove footer attribution

--- client/src/components/layout/Footer.jsx
+++ client/src/components/layout/Footer.jsx
@@ -1,7 +1,3 @@
 export default function Footer() {
-  return (
-    <footer className="footer">
-      Copyright © {new Date().getFullYear()} Permit Declaration · Project Develop by Yuvaraj M
-    </footer>
-  );
+  return <footer className="footer" />;
 }
